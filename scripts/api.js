79484bdd108b6067b1e4d93e8a826c276a8fea57
Lucas Batista da Silva <lucas_pivatto@hotmail.com.br>
2022-10-21 01:15:15 -0300

    funcionalidades do login implementadas

--- scripts/api.js
+++ scripts/api.js
@@ -1,1 +1,37 @@
-/* Desenvolva seu código aqui */
+const baseUrl = "http://localhost:3333/";
+
+const smallPassword = document.querySelector("#error-password");
+const inputEmail = document.querySelector("#email");
+const inputPassword = document.querySelector("#password");
+
+async function login(body) {
+  try {
+    const request = await fetch(`${baseUrl}login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    console.log(request);
+
+    if (request.ok) {
+      const response = await request.json();
+      localStorage.setItem("user", JSON.stringify(response));
+
+      setTimeout(() => {
+        window.location.href = "/pages/home/home.html";
+      }, 4000);
+    } else {
+      console.log("deu ruim");
+      smallPassword.classList = "password-error";
+      inputPassword.style.outline = "2px solid #c73650";
+      inputEmail.value = "";
+      inputPassword.value = "";
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export { login };
